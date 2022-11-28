0c7c4b2d77db63881f4528a491fd48a07d44c070
mmarckwo <78805766+mmarckwo@users.noreply.github.com>
2022-11-28 10:25:33 -0500

    added create recipe listeners - 11/28 MM
    add button adds more input fields and clicking submit will log each value in each input field

--- app/app.js
+++ app/app.js
@@ -1,13 +1,11 @@
 import * as MODEL from "./model.js";
 
-
 function initLinkListener() {
   $(window).on("hashchange", route);
   route();
 }
 
-
-function route(){
+function route() {
   let hashTag = window.location.hash;
   let pageID = hashTag.replace("#", "");
   let pageIDArray = pageID.split("/");
@@ -15,31 +13,71 @@ function route(){
   let subPageID = pageIDArray[1];
   console.log("sub" + subPageID);
 
-  if(pageID == ""){
-      MODEL.changePage("home");
+  if (pageID == "") {
+    MODEL.changePage("home");
   } else {
-      if (pageID == subPageID) {
-          MODEL.changePage(pageID)
-      } else if (pageID == "home"){
-        MODEL.changePage(pageID, subPageID);
-
-      }
-      else {
-          MODEL.changePage(pageID, subPageID);
-      }
+    if (pageID == subPageID) {
+      MODEL.changePage(pageID);
+    } else if (pageID == "home") {
+      MODEL.changePage(pageID, subPageID);
+    } else if (pageID == "createrecipe") {
+      MODEL.changePage(pageID, subPageID, createRecipeListeners);
+    } else {
+      MODEL.changePage(pageID, subPageID);
+    }
   }
 }
 
-function initApp(){
+function createRecipeListeners() {
+  var stepCount = 3;
+  var ingredientCount = 3;
+
+  $("#addStep").on("click", (e) => {
+    e.preventDefault();
+
+    stepCount++;
+
+    $(".steps").append(
+      `<input type="text" id="step${
+        stepCount - 1
+      }" placeholder="Step #${stepCount}" />`
+    );
+  });
+
+  $("#addIngredient").on("click", (e) => {
+    e.preventDefault();
+
+    ingredientCount++;
+
+    $(".ingred").append(
+      `<input type="text" id="ingred${
+        ingredientCount - 1
+      }" placeholder="Ingredient #${ingredientCount}" />`
+    );
+  });
+
+  $("#submitBtn").on("click", (e) => {
+    e.preventDefault();
+
+    for (let i = 0; i < stepCount; i++) {
+      let test = $(`#step${i}`).val();
+      console.log(test);
+    }
+
+    for (let i = 0; i < ingredientCount; i++) {
+      let test = $(`#ingred${i}`).val();
+      console.log(test);
+    }
+  });
+}
+
+function initApp() {
   $(window).on("hashchange", route);
   route();
 }
 
-
-
-
-
 $(document).ready(function () {
-  initLinkListener();
+  // initLinkListener and initApp are duplicate functions. change later.
+  //initLinkListener();
   initApp();
 });
--- app/model.js
+++ app/model.js
@@ -1,20 +1,24 @@
-
 export function changePage(pageID, subPageID, callback) {
-    if (pageID == "" || pageID == "home") {
-        $.get(`pages/home.html`, function (data) {
-            $("#app").html(data);
-            if (callback) {
-            callback();
-        }
-        });
-        } else if (subPageID != undefined) {
-            $.get(`pages/subpages/${subPageID}.html`, function (data) {
-              console.log("data " + data);
-                $("#app").html(data);
-            }); 
-        } else { $.get(`pages/${pageID}.html`, function (data) {
-            console.log("data " + data);
-                $("#app").html(data);
-        });
-        }
-    }
+  if (pageID == "" || pageID == "home") {
+    $.get(`pages/home.html`, function (data) {
+      $("#app").html(data);
+      if (callback) {
+        callback();
+      }
+    });
+    console.log("a");
+  } else if (subPageID != undefined) {
+    $.get(`pages/subpages/${subPageID}.html`, function (data) {
+      //console.log("data " + data);
+      $("#app").html(data);
+    });
+  } else {
+    $.get(`pages/${pageID}.html`, function (data) {
+      //console.log("data " + data);
+      $("#app").html(data);
+      if (callback) {
+        callback();
+      }
+    });
+  }
+}
